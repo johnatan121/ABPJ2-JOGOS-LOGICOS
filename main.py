2a880d704f4acfd1c79145bf0e023a05ffbe7faa
johnatan121 <johnatan121@live.com>
2026-02-12 21:57:19 -0300

    Add a quiz game with a question and attempts

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+print('ola seja bem vindo ao jogo de pergunta e resposta ')
+print(  """ /\_/\ 
+( o.o )
+ > ^ <   """)
+print('Qual é o menor país do mundo? ')
 
+resposta= 'vaticano'
+for tentativas in range(1,4):
+    usuario= input('\ndigite qual a sua resposta! ')
+    if usuario==resposta:
+        print('parabéns você acertou! ')
+    else:
+        print(f'\n(︶︹︶) voce errou! tente novamente, voce errou; {tentativas}') 
+        if tentativas==4:
+            print('aaa que pena sua tentativas acabaram')
